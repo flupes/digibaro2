871a0fc66425ff33af6dbe84006a395fc086aa6c
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2019-06-22 22:10:40 -0700

    re-work the memory map

--- lib/BaroUtils/src/flash_config.h
+++ lib/BaroUtils/src/flash_config.h
@@ -5,33 +5,40 @@
 
 class SPIFlash;
 
-// Flash memory types size (should be merged with BaroSample, etc.)
+// Flash memory types size 
+// TODO : should be merged with BaroSample, etc.
 constexpr uint32_t kRobustIndexByteLength = 4;
 constexpr uint32_t kRingSampleByteLength = 16;
 constexpr uint32_t kPermanentSampleBytesLength = 8;
 
 // Memory Map
+// Start of robust indexes
+// (Leave 4 sectors for settings)
 constexpr uint32_t kRobustIndexesSectorStart = 4;
 
-constexpr uint32_t kRobustIndexesSectorLength =
-    6;  // 2*(2+1) sectors --> 3072 indices
+// Robust indexes are stored on 6 sectors:
+// Redundancy = 2 / Need to erase one full sector when rotating
+// 6/2 * 4096 / 4 = 3072 total indices
+// --> 6/2 - 1 = 2 usable sectors --> (2*4096/4)=2048 usable indices
+constexpr uint32_t kRobustIndexesSectorLength = 6;
 
-constexpr uint32_t kRingSamplesSectorStart =
-    kRobustIndexesSectorStart + kRobustIndexesSectorLength;
-constexpr uint32_t kRingSamplesSectorLength =
-    12;  // 3072*16[sample_size] / 4096
-
+// RotatingSamples computes correctly number of sector required!
+// This value is hard coded here only for convinience purpose, and will 
+// only be valid if the RotatingSamples is constructed without optional
+// arguments.
 constexpr uint32_t kPermanentSamplesSectorStart =
-    kRingSamplesSectorStart + kRingSamplesSectorLength;
-// constexpr uint32_t kPermanentSamplesSectorLength = 490;
+    kRobustIndexesSectorStart + (1+2) * kRobustIndexesSectorLength;
+
+// 25.5 years of hourly samples
+// constexpr uint32_t kPermanentSamplesSectorLength = 436;
+
+// For debug purpose: 86 * 4096 / 8 = 44032 --> 5 years
 constexpr uint32_t kPermanentSamplesSectorLength = 86;
 
 constexpr uint32_t kDebugSectorStart =
     kPermanentSamplesSectorStart + kPermanentSamplesSectorLength;
 
 constexpr uint32_t KDebugSectorLength = 512 - kDebugSectorStart;
-
-extern uint32_t NextAvailableDebugAddr(SPIFlash &flash);
 
 constexpr uint8_t kMiniUltraProOnBoardChipSelectPin = 4;
 
